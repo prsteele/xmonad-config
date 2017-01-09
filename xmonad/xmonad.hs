8b3451c554ac6ae21e5a193113a754123540f66d
Patrick Steele <steele.pat@gmail.com>
2017-01-09 11:03:35 -0500

    Updated xmobar for the solarized color theme.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -108,12 +108,30 @@ myConfig = withUrgencyHook NoUrgencyHook desktopConfig {
   , XMonad.workspaces  = myWorkspaces
   } `additionalKeys` myKeys
 
+-- Solarized colors
+base03  = "#002b36"
+base02  = "#073642"
+base01  = "#586e75"
+base00  = "#657b83"
+base0   = "#839496"
+base1   = "#93a1a1"
+base2   = "#eee8d5"
+base3   = "#fdf6e3"
+yellow  = "#b58900"
+orange  = "#cb4b16"
+red     = "#dc322f"
+magenta = "#d33682"
+violet  = "#6c71c4"
+blue    = "#268bd2"
+cyan    = "#2aa198"
+green   = "#859900"
+
 -- xmobar configuration
-xmobarTitle = xmobarColor "#1589CE" "" . shorten 100
-xmobarCurrentWorkspace = xmobarColor "#1589CE" "" . wrap "[" "]"
-xmobarVisibleWorkspace = xmobarColor "#9B991F" ""
-xmobarHiddenWorkspace = xmobarColor "#9B991F" ""
-xmobarUrgent = xmobarColor "red" "yellow"
+xmobarTitle = xmobarColor blue "" . shorten 100
+xmobarCurrentWorkspace = xmobarColor blue "" . wrap "[" "]"
+xmobarVisibleWorkspace = xmobarColor yellow ""
+xmobarHiddenWorkspace = xmobarColor yellow ""
+xmobarUrgent = xmobarColor red ""
 
 myLogHook = xmobarPP {
   ppTitle = xmobarTitle
